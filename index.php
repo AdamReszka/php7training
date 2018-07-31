330571756ee4adf5ee967a6a8b225fd5d390befb
AdamReszka <adam.reszka85@gmail.com>
2018-07-31 23:26:18 +0200

    konkatenacja i stosowanie cudzysłowa i apostrofu

--- index.php
+++ index.php
@@ -2,4 +2,10 @@
 # konkatenacja
   $price = 899;
   $symbol = 'zł';
+
+# operatorem łączenia w php nie jest + jak w JS tylko kropka
+  echo 'Cena: ' . $price . $symbol;
+  echo "Cena: $price$symbol"; //tak można też konkatenować ale musi być cudzysłów a nie apostrof
+  $phrase = '"A to jest cytat z widocznym cudzysłowem"';
+  $english = 'It\'s a bit like the difference between'; //użycie apostrofu z ukośnikiem by był interpretowany jako zwykły znak
 ?>
