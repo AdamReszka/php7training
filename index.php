f739f2edbbaec7e89611cbc0bf50b4ee9a3a03e2
AdamReszka <adam.reszka85@gmail.com>
2018-08-01 22:05:45 +0200

    operatory arytmetyczne

--- index.php
+++ index.php
@@ -1,14 +1,3 @@
 <?php
-# tablice asocjacyjne posiadają dane w formacie klucz wartość
-$article = [
-  'id' => 1,
-  'title' => 'Wprowadzenie do Java',
-  'author' => 'Adam Reszka'
-];
-
-$article['category'] = 'Webdev'; //tak dodajemy nowe pary klucz wartość
-$article['views'] = 9547;
-
-print_r($article);
-echo $article['title']; //odwołanie się do pojedynczego klucza z tablicy
+# operatory arytmetyczne
 ?>
